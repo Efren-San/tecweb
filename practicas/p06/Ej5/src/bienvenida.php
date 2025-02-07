c29b538badf808a81c5c3237ea94ba2a0810b7cc
Efrén-San <joseefrensanchezl@gmail.com>
2025-02-06 22:31:28 -0600

    cambio de nombre en carpeta, correcciones hechas y ejercicio 5 agregado

---
+++ practicas/p06/Ej5/src/bienvenida.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Bienvenida</title>
+    </head>
+
+    <body>
+        <?php
+            if(isset($_POST["edad"]) && isset($_POST["sexo"]))
+            {
+                $edad=$_POST["edad"];
+                $sexo=$_POST["sexo"];
+
+                if($edad >= 18 && $edad <= 35 && $sexo=='femenino') {
+                    echo '<h3>Bienvenida, usted está en el rango de edad permitido. Su edad es: '. $edad .'</h3>';
+                }
+
+                else {
+                    echo '<h3>Error, los requisitos no cumplen lo establecido.</h3>';
+                }
+            }
+        ?>
+    </body>
+</html>
