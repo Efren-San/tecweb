2b9c747f5cfbf3e0f71fa09c89caf931e9d88cd1
Efrén-San <joseefrensanchezl@gmail.com>
2025-02-06 20:13:44 -0600

    funciones agregadas a funciones.php e index modificado

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -9,17 +9,10 @@
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
+        require_once __DIR__.'/src/funciones.php';
         if(isset($_GET['numero']))
         {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+            es_multiplo7y5($_GET['numero']);
         }
     ?>
 
@@ -33,10 +26,20 @@
     <?php
         if(isset($_POST["name"]) && isset($_POST["email"]))
         {
-            echo $_POST["name"];
-            echo '<br>';
-            echo $_POST["email"];
+            print_nombreyemail($_POST["name"], $_POST["email"]);
         }
+    ?>
+
+    
+    <h2>Ejercicio 2</h2>
+    <p>Crea un programa para la generación repetitiva de 3 números <br>
+       aleatorios hasta obtener una secuencia compuesta por:</p>
+    <p>impar, par, impar</p>
+    
+    <?php
+        $matriz=[];
+        $lim=1;
+        secuencia($matriz, $lim);
     ?>
 </body>
 </html>
