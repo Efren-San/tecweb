3489b0d099cd09149a88fa3442c25f3e08aaf919
Efrén-San <joseefrensanchezl@gmail.com>
2025-02-06 20:49:28 -0600

    ejercicio 2 corregido y mejorado

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -7,39 +7,40 @@
 </head>
 <body>
     <h2>Ejercicio 1</h2>
-    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
-    <?php
-        require_once __DIR__.'/src/funciones.php';
-        if(isset($_GET['numero']))
-        {
-            es_multiplo7y5($_GET['numero']);
-        }
-    ?>
+        <p>Programa para comprobar si un número es un múltiplo de 5 y 7</p>
+        <?php
+            require_once __DIR__.'/src/funciones.php';
+            if(isset($_GET['numero']))
+            {
+                multiplode7y5($_GET['numero']);
+            }
+        ?>
 
     <h2>Ejemplo de POST</h2>
-    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
+    <form action="http://localhost/tecweb/practicas/p06/index.php" method="post">
         Name: <input type="text" name="name"><br>
         E-mail: <input type="text" name="email"><br>
         <input type="submit">
     </form>
     <br>
-    <?php
-        if(isset($_POST["name"]) && isset($_POST["email"]))
-        {
-            print_nombreyemail($_POST["name"], $_POST["email"]);
-        }
-    ?>
+        <?php
+            if(isset($_POST["name"]) && isset($_POST["email"]))
+            {
+                print_nombreyemail($_POST["name"], $_POST["email"]);
+            }
+        ?>
 
     
     <h2>Ejercicio 2</h2>
-    <p>Crea un programa para la generación repetitiva de 3 números <br>
-       aleatorios hasta obtener una secuencia compuesta por:</p>
-    <p>impar, par, impar</p>
-    
-    <?php
-        $matriz=[];
-        $lim=1;
-        secuencia($matriz, $lim);
-    ?>
+    <p>Crea un programa para la generación repetitiva de 3 números aleatorios 
+       hasta obtener una secuencia compuesta por: <b>impar, par, impar</b></p>
+        <?php
+            $matriz=[];
+            $lim=1;
+            secuencia($matriz, $lim);
+        ?>
+
+
+
 </body>
 </html>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -1,21 +1,47 @@
 <?php
-  function es_multiplo7y5($num) {
+  function multiplo7dey5($num) {
     $num = $_GET['numero'];
     if ($num%5==0 && $num%7==0)
-    {
-      echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-    }
+        {
+        echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+        }
     else
-    {
-      echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-    }
+        {
+        echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+        }
   }
 
   function print_nombreyemail($name, $email) {
     $name=$_POST["name"];
     $email=$_POST["email"];
-
     echo $name;
     echo '<br>';
     echo $email;
   }
+
+  function secuencia($matriz, $lim) {
+    for ($i=0; $i<$lim; $i++) {
+      for ($j=0; $j<3; $j++) {
+          $Numval=mt_rand(100, 999);
+          $matriz[$i][$j] = $Numval;
+      }
+
+      if($matriz[$i][0]%2!=0 && $matriz[$i][1]%2==0 && $matriz[$i][2]%2!=0) {
+          break;
+      }
+      else {
+          $lim++;
+      }
+    }
+
+    for ($i=0; $i<$lim; $i++) {
+      for ($j=0; $j<3; $j++) {
+          echo $matriz[$i][$j].' ';
+      }
+      echo '<br>';
+    }
+  
+    echo '<br> <em>La cajita mágica</em> necesitó '. $i*3 .' números y ' . $i . 
+    ' iteración/es para encontrar el <b>impar, par e impar</b>';
+  }
+
