8eb96c8a9d6adf231faf0cd08c65a2ed1503e710
Efrén-San <joseefrensanchezl@gmail.com>
2025-04-29 22:29:23 -0600

    index.php creado

--- practicas/p12API/product_app/backend/product-delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    use Backend\MyApi\Delete\Delete as Delete;
-    require_once __DIR__.'/../vendor/autoload.php';
-
-    $productos = new Delete('marketzone');
-    $productos->delete( $_POST['id'] );
-    echo $productos->getData();
-?>
--- practicas/p12API/product_app/backend/product-single.php
+++
@@ -1,8 +0,0 @@
-<?php
-    use Backend\MyApi\Read\Read as Read;
-    require_once __DIR__.'/../vendor/autoload.php';
-
-    $productos = new Read('marketzone');
-    $productos->single( $_POST['id'] );
-    echo $productos->getData();
-?>
